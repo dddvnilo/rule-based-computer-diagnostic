94f4086967b0a8e24529139897469e6bf0e340cc
Danilo Drobnjak <drobnjakdm@gmail.com>
2026-05-22 10:23:39 +0200

    project setup

---
+++ kjar/src/main/java/com/ftn/sbnz/kjar/KjarApplication.java
@@ -0,0 +1,8 @@
+package com.ftn.sbnz.kjar;
+
+public class KjarApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ model/src/main/java/com/ftn/sbnz/model/ModelApplication.java
@@ -0,0 +1,8 @@
+package com.ftn.sbnz.model;
+
+public class ModelApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
